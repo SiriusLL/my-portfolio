1897fcf55c01fdcbd8f68846517af7d4addc053b
SiriusLL <sirius.love.light@gmail.com>
2021-08-17 23:56:48 -0600

    NavBar jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <>
+      <div>Resume</div>
+      <div>skills</div>
+      <div>Education</div>
+      <div>References</div>
+      <div>Contact</div>
+    </>
+  );
+};
+
+export default NavBar;
