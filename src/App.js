d391b13020aca5c2c46647c6c4885aefd1c0fb22
SiriusLL <sirius.love.light@gmail.com>
2021-08-19 22:38:58 -0600

    NavBar Menu Icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Portfolio</h2>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,45 @@
-const NavBar = () => {
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
     <>
-      <div>Resume</div>
-      <div>skills</div>
-      <div>Education</div>
-      <div>References</div>
-      <div>Contact</div>
+      <div className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            MyPortfolio
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nave-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                home
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
-};
+}
 
 export default NavBar;
+
+// const NavBar = () => {
+//   return (
+//     <>
+//       <div>Resume</div>
+//       <div>skills</div>
+//       <div>Education</div>
+//       <div>References</div>
+//       <div>Contact</div>
+//     </>
+//   );
+// };
+
+// export default NavBar;
